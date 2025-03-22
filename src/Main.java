8913523ac6566aae5ce5ced87701c1120607ddaa
RoronoaZoro1925 <sarthak1999vyas@gmail.com>
2025-03-22 15:23:41 +0530

    Complete file automate

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        FileReader r = new FileReader("E:\\ab.txt");
-        int i;
-        while((i=r.read())!=-1)
+    public static void main(String[] args) throws IOException
+    {
+        String filename = "E:\\ab.txt";
+        BufferedReader r = new BufferedReader(new FileReader(filename));
+        Set<String> colliID = new HashSet<>();
+        String i;
+        while((i=r.readLine())!=null)
         {
-            System.out.print((char) i);
+            i=i.trim();
+            if (!i.contains("UNKNOWN"))
+                colliID.add(i);
+
         }
+
+        for (String colli:colliID)
+            System.out.println(colli);
     }
 }
